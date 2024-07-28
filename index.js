d0b52b55198ef932e4d8f7a86beabfcdead32db6
Shahrukh Shamim <shahrukhshamim@live.com>
2024-07-28 23:41:24 +0500

    Node JS install

---
+++ index.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+});
